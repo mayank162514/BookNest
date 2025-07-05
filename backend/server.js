364179e4a2f6fef03047dc9b699dd531abe024a8
mayank162514 <130738930+mayank162514@users.noreply.github.com>
2025-07-05 12:26:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ console.log("Port is", process.env.PORT)
 const __dirname = path.resolve();
 
 //middlewares 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: "https://booknest-frontend-js7n.onrender.com", credentials: true }))
 app.use(express.json({ limit: '20mb' }))
 app.use(cookieParser())
 
